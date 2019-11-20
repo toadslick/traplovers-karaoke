0e8628fd372d3ac428479915b3ae615e2cd1e6a0
James Childress <james.childress@zapier.com>
2019-11-19 22:18:36 -0800

    comments about react-refetch in SongSearchResults

--- src/components/SongSearchResults.js
+++ src/components/SongSearchResults.js
@@ -7,11 +7,15 @@ import SongSearchListItem from './SongSearchListItem';
 const SongSearchList = ({ resultsFetch }) => {
   if (resultsFetch) {
     const { pending, rejected, fulfilled, value } = resultsFetch;
+
+    // If the request is ongoing, display the loading indicator.
     if (pending) {
       return <Loader />;
+      // Display an error message if the request has failed.
     } else if (rejected) {
       return <p>oh noes</p>;
-    } else if (fulfilled && value.items.length) {
+      // If the request has succeeded and has results, display the search results.
+    } else if (fulfilled && value.items && value.items.length) {
       return (
         <ul>
           {value.items.map(video => (
@@ -24,11 +28,15 @@ const SongSearchList = ({ resultsFetch }) => {
   return null;
 };
 
+// Return different react-refetch configs depending on the current search query.
+// If the search query is empty, return an empty result instead of making a request.
 const buildFetchRequest = query => {
   const trimmedQuery = query.trim();
   if (trimmedQuery) {
     const escapedQuery = encodeURI(`karaoke ${trimmedQuery}`);
     const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+    // If react-refetch receives an object with `{ url, headers, etc }`
+    // then it will make that HTTP request.
     return {
       url: `https://www.googleapis.com/youtube/v3/search?q=${escapedQuery}&key=${apiKey}&type=video&part=snippet&videoEmbeddable=true&maxResults=50`,
       headers: {
@@ -36,7 +44,8 @@ const buildFetchRequest = query => {
       },
     };
   }
-  // If the search query is empty, return a mocked empty set instead of performing a search
+  // If react-refetch receives an object with `{ value }`
+  // then it will return that value instead of making a request.
   return {
     value: {
       items: [],
@@ -44,6 +53,9 @@ const buildFetchRequest = query => {
   };
 };
 
+// This will add a `resultsFetch` prop to the SongSearchList component,
+// with `pending`, `rejected`, and `fulfilled` booleans to know the state of the request
+// and a `value` that is the result of the successful request.
 export default connect(({ query }) => ({
   resultsFetch: buildFetchRequest(query),
 }))(SongSearchList);
