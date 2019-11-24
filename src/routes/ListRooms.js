324b5fd35b319ee4a3c7763898dc37f157fcee80
Alex Migdol <amigdol@cvent.com>
2019-11-24 09:11:38 -0800

    add create room form and add link to direct user to create room form from room list

--- src/routes/ListRooms.js
+++ src/routes/ListRooms.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FirestoreCollection } from 'react-firestore';
 import Loader from '../components/Loader';
+import t from '../utils/translate';
 
-const ListRooms = () => {
-  const renderRoomList = ({ id, roomName, roomId }) => (
-    <li key={id}>
-      <Link to={`/room/${roomId}`}>{roomName}</Link>
-    </li>
-  );
+const RoomListItem = ({ id, roomName, roomId }) => (
+  <li key={id}>
+    <Link to={`/room/${roomId}`}>{roomName}</Link>
+  </li>
+);
 
-  return (
-    <>
-      <h2>Rooms</h2>
-      <FirestoreCollection
-        path={'publicRoomListings'}
-        render={({ isLoading, data }) => {
-          return isLoading ? <Loader /> : <ul>{data.map(renderRoomList)}</ul>;
-        }}
-        sort="sortKey"
-      />
-    </>
-  );
-};
+const ListRooms = () => (
+  <>
+    <h2>Rooms</h2>
+    <FirestoreCollection
+      path={'publicRoomListings'}
+      render={({ isLoading, data }) => {
+        return isLoading ? <Loader /> : <ul>{data.map(RoomListItem)}</ul>;
+      }}
+      sort="sortKey"
+    />
+    <Link to="/room/new">{t('addRoomLink')}</Link>
+  </>
+);
 
 export default ListRooms;
