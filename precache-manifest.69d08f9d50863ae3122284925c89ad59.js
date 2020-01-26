7c290283365bc274bc25321a8d600b73eedc6320
James Childress <james.childress@zapier.com>
2020-01-26 14:44:48 -0800

    Updates

---
+++ precache-manifest.69d08f9d50863ae3122284925c89ad59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfd13b0f50270f2fa01fcb503b48f04",
+    "url": "/traplovers-karaoke/index.html"
+  },
+  {
+    "revision": "5c8d449643adb26d2c28",
+    "url": "/traplovers-karaoke/static/css/main.87fbe6f4.chunk.css"
+  },
+  {
+    "revision": "4ad0e6eb6e90f8de1f3f",
+    "url": "/traplovers-karaoke/static/js/2.d92e3ac8.chunk.js"
+  },
+  {
+    "revision": "5c8d449643adb26d2c28",
+    "url": "/traplovers-karaoke/static/js/main.45a88b03.chunk.js"
+  },
+  {
+    "revision": "26f4ee6ec3adbf85fe0f",
+    "url": "/traplovers-karaoke/static/js/runtime-main.775dbd99.js"
+  }
+]);
